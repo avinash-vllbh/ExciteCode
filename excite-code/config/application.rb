43e7e12ce2ed909576d5fc776182c99d5542576b
avinash-vllbh <avallab1@asu.edu>
2014-07-08 12:09:58 -0700

    Added bootstrap 3.2

--- excite-code/config/application.rb
+++ excite-code/config/application.rb
@@ -19,5 +19,8 @@ module ExciteCode
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #To include bootstrap fonts in vendor/assets folder
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
