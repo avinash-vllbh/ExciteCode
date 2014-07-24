105974c4262866306e79711b422264f4d84683ef
avinash-vllbh <avallab1@asu.edu>
2014-07-24 15:47:15 -0700

    Took care of comments redirect exceptions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,13 @@
 class CommentsController < ApplicationController
+  def index
+    begin
+      @blog = Blog.find(params[:blog_id])
+      redirect_to blog_path(@blog)
+    rescue ActiveRecord::RecordNotFound
+      flash[:notice] = "I don't have that. Are you sure?"
+      redirect_to blog_index_path
+    end
+  end
   def create
     @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.create(comment_params)
