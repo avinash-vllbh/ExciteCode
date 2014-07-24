06dbe61a5e977d9122cccbd9b30f70f9374ba13b
avinash-vllbh <avallab1@asu.edu>
2014-07-24 16:40:19 -0700

    added stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
+  get 'blog/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'blog#index'
   resources :blog do
     resources :comments
   end
