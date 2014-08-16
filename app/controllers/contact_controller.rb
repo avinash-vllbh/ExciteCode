85d82a2e674a5a1bd9462645b6edef060bbac76f
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-08-16 12:03:01 -0700

    updated footer for the web app

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,13 +5,14 @@ class ContactController < ApplicationController
   def create_contact
     @contact = Contact.new(contact_parmas)
     if @contact.save
-      flash[:notice] = "successful"
+      flash[:notice] = "Thank you... will get back as soon as I can"
       redirect_to root_path
     else
       flash[:alert] = "failed"
       render "contact"
     end
   end
+
   
   private
   def contact_parmas
