a7c49f11413b9ebcc83341b940f13e48b15b21f1
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-10-20 15:26:02 -0700

    using capistrano/rails/assets rather than compile_assets_locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,6 +67,8 @@ set(:symlinks, [
   }
 ])
 
+set :assets_roles, [:app]
+
 # this:
 # http://www.capistranorb.com/documentation/getting-started/flow/
 # is worth reading for a quick overview of what tasks are called
@@ -78,7 +80,7 @@ namespace :deploy do
   # only allow a deploy with passing tests to deployed
   before :deploy, "deploy:run_tests"
   # compile assets locally then rsync
-  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  # after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
 
   # remove the default nginx configuration as it will tend
